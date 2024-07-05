6345750fd5caedff2ed55eb41378da7abdcf0c86
t4e1 <jtw5738@naver.com>
2024-07-05 18:02:03 +0900

    Feat: 프로젝트 생성 업로드

---
+++ webtoon-search-back/webtoon-search/src/main/java/com/samsamohoh/webtoonsearch/WebtoonSearchApplication.java
@@ -0,0 +1,13 @@
+package com.samsamohoh.webtoonsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonSearchApplication.class, args);
+	}
+
+}
---
+++ webtoon-search-back/webtoon-search/src/test/java/com/samsamohoh/webtoonsearch/WebtoonSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsamohoh.webtoonsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoonSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
