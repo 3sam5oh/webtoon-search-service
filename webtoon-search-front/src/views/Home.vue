4bf3998968f3bbc615927013c8a10d68897330f7
koyoung99 <oayoung99@naver.com>
2024-08-20 18:57:47 +0900

    feat (search) : front와 back search 메소드 연동
    - local환경테스트

--- webtoon-search-front/src/views/Home.vue
+++ webtoon-search-front/src/views/Home.vue
@@ -9,23 +9,10 @@ export default {
       authenticated: false,
       user: {},
       searchWord: null,
-      selectedDay: null,
-      week: ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"],
-      todayDay: "",// 오늘 요일을 저장
-      contentVisible: true,
+      webtoonList: [],
     };
   },
-  computed: {
-    ageInt() {
-      return parseInt(this.user.age, 10);
-    }
-  },
   methods: {
-    getTodayDay() {
-      const days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-      const today = new Date();
-      this.todayDay = days[today.getDay()]; // 오늘의 요일을 설정
-    },
     checkAuthentication() {
       axios.get('http://localhost:8080/logins/loginInfo', {withCredentials: true})
           .then(response => {
@@ -37,16 +24,34 @@ export default {
           });
     },
     search() {
-      this.contentVisible = false;
+      console.log(this.searchWord);
+      axios.get('http://localhost:8080/webtoons/search', {
+        params: {
+          query: this.searchWord  // 서버로 검색어를 query 파라미터로 전송
+        }
+      })
+          .then(response => {
+            // 전체 응답 데이터 구조를 출력하여 확인
+            console.log('Inner Data:', response.data.data);
 
-    },
-    goToSite(url) {
-      window.open(url, '_blank'); // 새 창 또는 새 탭에서 URL 열기
+            // 데이터가 있는지 확인하고 webtoonList에 저장
+            if (response.data && response.data.data && Array.isArray(response.data.data.webtoons)) {
+              this.webtoonList = response.data.data.webtoons;
+            } else {
+              console.error('Invalid data structure');
+              this.webtoonList = [];  // 빈 배열로 초기화
+            }
+
+            console.log('Webtoon List:', this.webtoonList);
+          })
+          .catch(error => {
+            console.error('Failed to fetch search info:', error);
+            this.webtoonList = [];  // 오류 발생 시 빈 배열로 초기화
+          });
     }
   },
   created() {
     this.checkAuthentication(); // 페이지가 로드될 때 로그인 상태를 확인합니다.
-    this.getTodayDay(); // 오늘의 요일을 가져옴
   }
 }
 </script>
@@ -56,14 +61,21 @@ export default {
   <div class="wrap">
     <header class="header">
       <div class="headerName">
-        <div class="headerName1">3SAM5OH</div>
-        <div class="headerName2">webtoon recommendation</div>
+        <a>
+          <div class="headerName1">3SAM5OH</div>
+          <div class="headerName2">webtoon recommendation</div>
+        </a>
       </div>
 
       <!-- 검색버튼 -->
       <div class="search-container">
-        <input type="text" placeholder="검색어를 입력하세요" v-model="searchWord">
-        <button type="button" @click="search" class="search-button"></button>
+        <form @submit.prevent="search">  <!-- submit 시 search 메서드 호출 -->
+          <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              v-model="searchWord">
+          <button type="submit" class="search-button"></button>
+        </form>
       </div>
 
 
@@ -82,137 +94,34 @@ export default {
       </div>
 
     </header>
-    <div class="content" v-if="contentVisible">
+    <body>
 
-      <!-- 회원정보 -->
-      <div style=" font-size: 22px;
-    font-weight: bold;" v-if="Object.keys(user).length <= 0"># 이달의 추천
+    <div class="contentFind">
+      <div v-if="webtoonList.length > 0">
+        <div v-for="webtoon in webtoonList" :key="webtoon.id" class="image-container">
+          <div>
+            <!-- thumbnail이 존재하고 요소가 있을 때만 이미지를 렌더링 -->
+            <img v-if="webtoon.thumbnail && webtoon.thumbnail.length > 0" :src="webtoon.thumbnail[0]" alt="웹툰 이미지">
+            <img v-else src="@/assets/images/noimage.png" alt="기본 이미지">  <!-- 기본 이미지 설정 -->
+          </div>
+          <div class="webtoonContainer">
+            <div class="webtoonTitle">{{ webtoon.title }}</div>
+            <div class="imageInfo">
+              <div> {{ webtoon.provider }} </div>
+              <div>{{ webtoon.authors.join(', ') }}</div>  <!-- 작가 리스트를 쉼표로 구분 -->
+              <div>{{ webtoon.ageGrade === 0 ? '전체 이용가' : webtoon.ageGrade + '세 이용가' }}</div>
+              <div v-if="webtoon.end">완결</div>
+              <div v-else>연재 중</div>
+            </div>
+            <a :href="webtoon.url" target="_blank" style="font-size: 16px; font-weight: bold">웹툰 보러가기</a>
+          </div>
+        </div>
       </div>
-      <div class="memberInfo" v-if="Object.keys(user).length > 0">
-        <!-- {{ 나이 user.age }} -->
-        <div>
-          <div v-if="ageInt >=10 && ageInt <=19 "> # 10대</div>
-          <div v-if="ageInt >=20 && ageInt <=29 "> # 20대</div>
-          <div v-if="ageInt >=30 && ageInt <=39 "> # 30대</div>
-          <div v-if="ageInt >=40 && ageInt <=49 "> # 40대</div>
-          <div v-if="ageInt >=50 && ageInt <=59 "> # 50대</div>
-        </div>
-
-        <!--성별 {{user.gender}}-->
-        <div>
-          <div v-if="user.gender == 'F'"> 여성 추천</div>
-          <div v-if="user.gender == 'M'"> 남성 추천</div>
-        </div>
-      </div>
-      <div class="webtoonRecommend">
-
-        <div class="image-container">
-          <img
-              src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg">
-          <label>폭력의 왕</label>
-        </div>
-        <div class="image-container">
-          <img
-              src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg">
-          <label>폭력의 왕</label>
-        </div>
-        <div class="image-container">
-          <img
-              src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg">
-          <label>폭력의 왕</label>
-        </div>
-        <div class="image-container">
-          <img
-              src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg">
-          <label>폭력의 왕</label>
-        </div>
-
-      </div>
-
-      <div style="font-size: 22px; font-weight: bold; margin-bottom: 20px">요일별 전체웹툰</div>
-
-      <div class="webtoonList">
-        <div
-            v-for="(day, index) in week"
-            :key="index"
-            class="day-container"
-            :class="{ 'highlight-today1': todayDay === day }">
-          <div class="day-header" :class="{ 'highlight-today2': todayDay === day }">
-            {{ day }}
-          </div>
-          <div class="image-container" @click="goToSite('https://comic.naver.com/webtoon/list?titleId=824890')">
-            <img
-                src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg"
-            />
-            <label>폭력의 왕</label>
-          </div>
-          <div class="image-container" @click="goToSite('https://comic.naver.com/webtoon/list?titleId=824890')">
-            <img
-                src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg"
-            />
-            <label>폭력의 왕</label>
-          </div>
-          <div class="image-container" @click="goToSite('https://comic.naver.com/webtoon/list?titleId=824890')">
-            <img
-                src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg"
-            />
-            <label>폭력의 왕</label>
-          </div>
-          <div class="image-container" @click="goToSite('https://comic.naver.com/webtoon/list?titleId=824890')">
-            <img
-                src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg"
-            />
-            <label>폭력의 왕</label>
-          </div>
-          <div class="image-container" @click="goToSite('https://comic.naver.com/webtoon/list?titleId=824890')">
-            <img
-                src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg"
-            />
-            <label>폭력의 왕</label>
-          </div>
-          <div class="image-container" @click="goToSite('https://comic.naver.com/webtoon/list?titleId=824890')">
-            <img
-                src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg"
-            />
-            <label>폭력의 왕</label>
-          </div>
-        </div>
+      <div class="contentNotFind" v-else>
+        <p>검색 결과가 없습니다.</p>
       </div>
     </div>
-    <div class="contentFind" v-else>
-      <div style="font-size: 22px; padding: 10px; margin-bottom: 20px; border-bottom:1px solid #d2d2d2">
-        <strong>'{{ searchWord }}'</strong> 에 대한 검색결과입니다.
-      </div>
-
-      <div class="image-container">
-        <div>
-          <img
-              src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg">
-        </div>
-        <div class="webtoonContainer">
-          <div class="webtoonTitle">폭력의 왕</div>
-          <div class="imageInfo">
-            <div>누굴까?</div>
-            <div>월요일</div>
-            <div>전체이용가</div>
-          </div>
-        </div>
-      </div>
-      <div class="image-container">
-        <div>
-          <img
-              src="https://image-comic.pstatic.net/webtoon/824890/thumbnail/thumbnail_IMAG21_95e7706d-f30f-484a-ae56-46532f7711f6.jpg">
-        </div>
-        <div class="webtoonContainer">
-          <div class="webtoonTitle">폭력의 왕</div>
-          <div class="imageInfo">
-            <div>누굴까?</div>
-            <div>월요일</div>
-            <div>전체이용가</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </body>
 
   </div>
 </template>
