0c774e9273be9cf359a481c037d21fca3c5a1137
DESKTOP-QQJSUA6\JTW <jtw5738@naver.com>
2024-11-12 15:15:41 +0900

    [refactor]

--- webtoon-search-back/webtoon-search/src/main/java/com/samsamohoh/webtoonsearch/application/port/in/webtoon/SelectRecordUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.samsamohoh.webtoonsearch.application.port.in.webtoon;
-
-import com.samsamohoh.webtoonsearch.application.port.in.webtoon.dto.SelectWebtoonRequest;
-
-public interface SelectRecordUseCase {
-
-    boolean addClickRecord(SelectWebtoonRequest selectWebtoonRequest);
-}
--- webtoon-search-back/webtoon-search/src/main/java/com/samsamohoh/webtoonsearch/application/port/out/AddRecordPort.java
+++
@@ -1,7 +0,0 @@
-package com.samsamohoh.webtoonsearch.application.port.out;
-
-import com.samsamohoh.webtoonsearch.application.port.in.webtoon.dto.SelectWebtoonRequest;
-
-public interface AddRecordPort {
-    boolean addRecord(SelectWebtoonRequest dto);
-}
